5c6767a0c20e5131c71af8efea9f29b81a593734
aleeIsaev <75799767+aleeIsaev@users.noreply.github.com>
2021-08-19 17:09:06 +0600

    add static files for heroku

--- chat/views.py
+++ chat/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from chat.models import Room, Message
 from django.http import HttpResponse, JsonResponse
 
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
 
 def room(request, room):
     username = request.GET.get('username')
@@ -15,16 +17,18 @@ def room(request, room):
         'room_details': room_details
     })
 
+
 def checkview(request):
     room = request.POST['room_name']
     username = request.POST['username']
 
     if Room.objects.filter(name=room).exists():
-        return redirect('/'+room+'/?username='+username)
+        return redirect('/' + room + '/?username=' + username)
     else:
         new_room = Room.objects.create(name=room)
         new_room.save()
-        return redirect('/'+room+'/?username='+username)
+        return redirect('/' + room + '/?username=' + username)
+
 
 def send(request):
     message = request.POST['message']
@@ -35,8 +39,9 @@ def send(request):
     new_message.save()
     return HttpResponse('Message sent successfully')
 
+
 def getMessages(request, room):
     room_details = Room.objects.get(name=room)
 
     messages = Message.objects.filter(room=room_details.id)
-    return JsonResponse({"messages":list(messages.values())})
+    return JsonResponse({"messages": list(messages.values())})
--- djangochat/settings.py
+++ djangochat/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -25,7 +27,8 @@ SECRET_KEY = 'm%c30v(4=6q!_ouzf5xx9l7n89pl0#l16lo6+z*g4!49-63@t#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['secretchatforus.herokuapp.'
+                 'com', '127.0.0.1']
 
 
 # Application definition
@@ -118,4 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
